f0064c90bbdccdff088448d948fc0ffcf7f1c971
yahyaqr <aqrom.yahya75@gmail.com>
2023-04-19 03:51:24 +0700

    Add vibration to server.js

--- migrations/migrations.js
+++ migrations/migrations.js
@@ -26,11 +26,11 @@ exports.up = (pgm) => {
       language: 'plpgsql',
     },
     `
-    BEGIN
-      NEW.primary_key := CONCAT(NEW.device_id, '_', NEW.timestamp);
-      RETURN NEW;
-    END;
-    `,
+      BEGIN
+        NEW.primary_key := CONCAT(NEW.device_id, '_', NEW.timestamp);
+        RETURN NEW;
+      END;
+      `,
   );
 
   // attach the trigger function to the rpm table
@@ -40,8 +40,46 @@ exports.up = (pgm) => {
     level: 'ROW',
     function: 'set_rpm_primary_key',
   });
+
+  // create the vibration table
+  pgm.createTable('vibration', {
+    timestamp: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    device_id: { type: 'varchar(10)', notNull: true },
+    vibration: { type: 'float', notNull: true },
+    primary_key: { type: 'string', notNull: true },
+  });
+
+  // create the trigger function to set the default value of the primary key column
+  pgm.createFunction(
+    'set_vibration_primary_key',
+    [],
+    {
+      replace: true,
+      returns: 'trigger',
+      language: 'plpgsql',
+    },
+    `
+    BEGIN
+      NEW.primary_key := CONCAT(NEW.device_id, '_', NEW.timestamp);
+      RETURN NEW;
+    END;
+    `,
+  );
+
+  // attach the trigger function to the vibration table
+  pgm.createTrigger('vibration', 'set_primary_key_trigger', {
+    when: 'BEFORE',
+    operation: 'INSERT',
+    level: 'ROW',
+    function: 'set_vibration_primary_key',
+  });
 };
 
 exports.down = (pgm) => {
   pgm.dropTable('rpm');
+  pgm.dropTable('vibration');
 };
--- src/server.js
+++ src/server.js
@@ -1,23 +1,32 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
-const rpm = require('./api/rpm');
 const dashboard = require('./api/dashboard');
+
+const rpm = require('./api/rpm');
 const PostgresRPMService = require('./services/postgres/RPMService');
 const InMemoryRPMService = require('./services/inMemory/RPMService');
 const RPMValidator = require('./validator/rpm');
 
+const vibration = require('./api/vibration');
+const PostgresVibrationService = require('./services/postgres/VibrationService');
+const InMemoryVibrationService = require('./services/inMemory/VibrationService');
+const VibrationValidator = require('./validator/vibration');
+
 const init = async () => {
   let rpmService;
+  let vibrationService;
 
   try {
     // Try to connect to PostgreSQL
     rpmService = new PostgresRPMService();
+    vibrationService = new PostgresVibrationService();
     console.log('Connected to PostgreSQL');
   } catch (error) {
     console.error('Failed to connect to PostgreSQL:', error.message);
     console.warn('Falling back to in-memory service');
     rpmService = new InMemoryRPMService();
+    vibrationService = new InMemoryVibrationService();
   }
 
   const server = Hapi.server({
@@ -39,6 +48,14 @@ const init = async () => {
     options: {
       service: rpmService,
       validator: RPMValidator,
+    },
+  });
+
+  await server.register({
+    plugin: vibration,
+    options: {
+      service: vibrationService,
+      validator: VibrationValidator,
     },
   });
 
